0f17acecbc766bd8db905dadc6f2daf2e261b2e8
Pradeep-Kumar-Rebbavarapu <wardengaming1807@gmail.com>
2024-11-13 18:51:02 +0530

    added production required files:

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from django.urls import path,include,re_path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('backend/admin/', admin.site.urls),
@@ -24,4 +28,6 @@ urlpatterns = [
     path('backend/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('backend/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('backend/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-]
+    re_path('backend/' + r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    re_path('backend/' + r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT})
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
