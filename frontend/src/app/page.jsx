4241169bbc211c7d8467033efd2eda96293e3af0
Pradeep-Kumar-Rebbavarapu <wardengaming1807@gmail.com>
2024-12-09 23:01:32 +0530

    changed all pages to client

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,12 +1,23 @@
+import { useState, useEffect } from "react";
 import { getMatches } from "@/api";
 import { ShowComponents } from "@/components/home";
 
-export default async function Page() {
-  const matches = await getMatches({ limit: 20 });
-  console.log('matches',matches)
+export default function Page() {
+  const [matches, setMatches] = useState([]);
+
+  useEffect(() => {
+    const fetchMatches = async () => {
+      const result = await getMatches({ limit: 20 });
+      setMatches(result);
+      console.log('matches', result);
+    };
+
+    fetchMatches();
+  }, []);
+
   return (
     <div>
-  <ShowComponents matches={matches} />
-  </div>
-  )
+      <ShowComponents matches={matches} />
+    </div>
+  );
 }
